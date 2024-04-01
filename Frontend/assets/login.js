256d59e239b1293020c35ba65449d2b0047b71dd
Corentin Charmeux <charmeux.corentin.pro@gmail.com>
2024-04-01 11:00:32 +0200

    fix: Correction du login et ajout d'erreur

--- Frontend/assets/login.js
+++ Frontend/assets/login.js
@@ -4,15 +4,20 @@ const errorMessage = document.getElementById("error_message")
 const inputs = document.querySelectorAll("input");
 const loginBtn = document.querySelector(".form");
 const form = document.querySelector("form");
+let existingError = false
+let messageOfError = ''
 
 const Error = (errorName, id) => {
-    let existingError = document.getElementById(id)
     if(!existingError) {
         let errorValue = document.createElement('p')
         errorMessage.appendChild(errorValue)
         errorValue.innerHTML = errorName
         errorValue.id = id
-    } 
+        messageOfError = errorValue
+        existingError = true
+    } else {
+        messageOfError.innerHTML = errorName
+    }
 }
 
 loginUl.addEventListener('click', () => {
@@ -24,18 +29,24 @@ loginBtn.addEventListener("submit", async (event) => {
     event.preventDefault();
     const email = event.target.email.value
     const password = event.target.password.value
-    const response = await fetch(apiLogin, {
+    const res = await fetch(apiLogin, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
     });
-    if (response.ok) {
-        const data = await response.json();
+    if (res.ok) {
+        const data = await res.json();
         localStorage.setItem("token", data.token);
         window.location.href = "./index.html"
-    }  else if(email !== response.ok || password !== response.ok) {
-        errorMessage.style.display = 'flex';
-        Error("Nom d'utilisateur ou mot de passe invalide.")
+    } else {
+        if(res.status === 404) {
+            errorMessage.style.display = 'flex';
+            Error("Nom d'utilisateur invalide.")
+        } else if (res.status === 401) {
+            errorMessage.style.display = 'flex';
+            Error("Mot de passe invalide.")
+        }
+        
     }
 
 });
